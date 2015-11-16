deaebc2053704fc5f5d447080a1a5cf9e6c334f0
Yaro <yaro.vasilyev@gmail.com>
2015-11-16 19:05:16 +0300

    Projects migrated to .NET 4.5 Full path to signtool specified in signfile.bat

--- src/signfile.bat
+++ src/signfile.bat
@@ -1,5 +1,5 @@
 @rem echo off
 if "%SIGNPDN%" == "1" (
-    signtool sign %PDNPFXARG% /d "Paint.NET" /du "http://www.getpaint.net/" /t http://timestamp.comodoca.com/authenticode /v "%1" 
+    "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe" sign %PDNPFXARG% /d "Paint.NET" /du "http://www.getpaint.net/" /t http://timestamp.comodoca.com/authenticode /v "%1" 
 )
 
